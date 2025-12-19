c12dbe7e9e6b11b3793564074f935f8e347e613a
jojomuldrow <johari.muldrow@gmail.com>
2025-12-19 16:52:47 -0500

    change visitor position

--- public/layout.js
+++ public/layout.js
@@ -41,7 +41,7 @@ function headerHTML() {
 	  <img class="image" alt="hk angel gif" src="https://i.imgur.com/nMVuaOk.gif" style="width: 90%"/>
         <div class="sidebar-section">
           <div class="sidebar-title">hits & current visitors</div>
-          <blockquote><a style="position:fixed;z-index:1339; !important;top:55px;left:3px;" href="http://www.snazzyspace.com/generators/viewer-counter/" title="SnazzySpace.com Viewer Counter" target="_blank">
+          <blockquote><a style="position:static;z-index:1339; !important;top:55px;left:3px;" href="http://www.snazzyspace.com/generators/viewer-counter/" title="SnazzySpace.com Viewer Counter" target="_blank">
           <img src="http://www.snazzyspace.com/generators/viewer-counter/counter.php/fid=1766180608/style=6/counter.png" border="0"></a></blockquote>
         </div>
         <div class="sidebar-section">
